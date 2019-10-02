d64973d4ba003e9f242e7b4ec06cb798e57af08d
Milzam Hibatullah <hmilzam@gmail.com>
2019-10-02 11:35:24 +0700

    split string of api key

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as client;
 
 Future<News> fetchNews() async {
+  var _apiKey ='d641f36aa8b7450cb3ebe7fbc1e69917';
   final response = await client.get(
-      'https://newsapi.org/v2/top-headlines?country=id&apiKey=d641f36aa8b7450cb3ebe7fbc1e69917');
+      'https://newsapi.org/v2/top-headlines?country=id&apiKey='+_apiKey);
   if (response.statusCode == 200) {
     print(response.body);
     return News.fromJson(json.decode(response.body));
